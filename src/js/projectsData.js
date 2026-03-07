0d331b6539300231da40a1fd1b9770d0d1f05cb4
Bubuka <justintisbe2@example.com>
2026-03-07 23:51:55 +0800

    last

--- src/js/projectsData.js
+++ src/js/projectsData.js
@@ -1,49 +1,49 @@
+import coffeerush from "../assets/imgs/coffeerush.png";
+import ists from "../assets/imgs/ists.svg";
+import suntrackingsolarpanel from "../assets/imgs/suntrackingsolarpanel.svg";
+import pharmatrack from "../assets/imgs/pharmatrack.svg";
+import reactTailwind from "../assets/imgs/react+tailwind.svg";
+
 export const projectsData = [
   {
     title: "☕ Coffee Rush 💸",
-    description:
-      "A Roblox game where players craft unique coffee recipes and serve NPCs using guided brewing systems.",
-    img: "../src/assets/imgs/coffeerush.png",
+    description: "A Roblox game where players craft unique coffee recipes and serve NPCs using guided brewing systems.",
+    img: coffeerush,
     link: "https://www.roblox.com/games/2957036843/Coffee-Rush",
     status: "Testing Phase",
   },
-
   {
     title: "Inventory & Sales Tracking System",
-    description:
-      "A web-based system for real-time product tracking, sales report, incident handling and more feautures.",
-    img: "../src/assets/imgs/ists.svg",
+    description: "A web-based system for real-time product tracking, sales report, incident handling and more feautures.",
+    img: ists,
     link: "",
     status: "Coding Phase",
   },
   {
     title: "Sun Tracking Solar Panel",
-    description:
-      "An IoT-based project using 2 LDR sensors to track sun for maximum efficiency using Servo Motor and ESP82.",
-    img: "../src/assets/imgs/suntrackingsolarpanel.svg",
+    description: "An IoT-based project using 2 LDR sensors to track sun for maximum efficiency using Servo Motor and ESP82.",
+    img: suntrackingsolarpanel,
     link: "",
     status: "Coding Phase",
   },
   {
     title: "Stock Market Tracker",
-    description:
-      "Stock Market Tracker is a Flutter app that shows live stock prices, trend updates, and real time data from an API. It includes AI integration to give you simple insights, price summaries, and quick guidance based on market movement. It delivers clear information in a clean and fast layout.",
-    img: "blank",
+    description: "Stock Market Tracker is a Flutter app that shows live stock prices, trend updates, and real time data from an API. It includes AI integration to give you simple insights, price summaries, and quick guidance based on market movement. It delivers clear information in a clean and fast layout.",
+    img: "",
     link: "",
     status: "Coding Phase",
   },
   {
     title: "Pharmatrack",
-    description:
-      "PharmaTrack is designed by showing medication information by displaying medicine data such as uses, expiry dates, and necessary cautions.",
-    img: "../src/assets/imgs/pharmatrack.svg",
+    description: "PharmaTrack is designed by showing medication information by displaying medicine data such as uses, expiry dates, and necessary cautions.",
+    img: pharmatrack,
     link: "https://skelemete.github.io/pharmatrack/",
     status: "Done",
   },
   {
     title: "Portfolio Website",
     description: "This React + Tailwind portfolio.",
-    img: "../src/assets/imgs/react+tailwind.svg",
+    img: reactTailwind,
     link: "linkhere",
     status: "Deployed",
   },
